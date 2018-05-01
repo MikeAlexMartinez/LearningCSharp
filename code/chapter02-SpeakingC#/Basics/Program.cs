d47c95b712f8fafc60f1790f20667a1b9980e3b3
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2018-05-01 10:03:07 +0100

    upto write code to explore numbers

--- code/chapter02-SpeakingC#/Basics/Program.cs
+++ code/chapter02-SpeakingC#/Basics/Program.cs
@@ -1,16 +1,37 @@
-﻿using System.Console;
-using Syst
+﻿using static System.Console;
+using System.Linq;
+using System.Reflection;
 
 namespace Basics
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            WriteLine("Hello World!");
-            WriteLine();
+      System.Data.DataSet ds;
+      System.Net.Http.HttpClient client;
+      
+      WriteLine("Hello World!");
+      WriteLine("Hello Agani!");
 
-            // loop through
+      // loop through the assemblies that this application references
+      foreach (var r in Assembly.GetEntryAssembly()
+        .GetReferencedAssemblies())
+      {
+        // load the assembly so we can read its details
+        var a = Assembly.Load(new AssemblyName(r.FullName));
+        // declare a variable to count the total number of methods
+        int methodCount = 0;
+        // loop through all the types in the assembly
+        foreach (var t in a.DefinedTypes)
+        {
+          /// add up the counts of methods
+          methodCount += t.GetMethods().Count();
         }
+        // output the count of types and their methods
+        WriteLine($"{a.DefinedTypes.Count():N0} types " +
+          $"with {methodCount:N0} methods in {r.Name} assembly.");
+      }
     }
+  }
 }
