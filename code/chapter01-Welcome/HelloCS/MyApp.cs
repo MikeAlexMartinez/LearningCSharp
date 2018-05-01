dc577525b35814676abf3491c889f384702b513e
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2018-05-01 09:21:28 +0100

    Initial Commit

---
+++ code/chapter01-Welcome/HelloCS/MyApp.cs
@@ -0,0 +1,9 @@
+using static System.Console;
+
+namespace WelcomeDotNetCore {
+  class MyApp {
+    static void  Main() {
+      WriteLine("Welcome, .NET Core!");
+    }
+  }
+}
---
+++ code/chapter02-SpeakingC#/Basics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Console;
+using Syst
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine();
+
+            // loop through
+        }
+    }
+}
